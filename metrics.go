7644f8be3b32132cba9ce3bcb1ddbde8703d37ef
Nuno Morais <nm.morais@campus.fct.unl.pt>
2020-06-25 15:22:54 +0100

    add more metrics

--- metrics.go
+++ metrics.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
@@ -8,23 +9,64 @@ import (
 )
 
 var (
+	tileMetrics      = make(map[string]prometheus.Gauge)
 	connectedClients = promauto.NewGauge(prometheus.GaugeOpts{
 		Name: "location_connected_clients",
 		Help: "The total number of connected clients",
 	})
 )
 
+func emitNrConnectedClients() {
+	counter := 0
+	clientChannels.Range(func(_, _ interface{}) bool {
+		counter++
+		return true
+	})
+	connectedClients.Set(float64(counter))
+}
+
+func emitNrConnectedTrainersInTile(tileNr int, tile Tile) {
+	name := fmt.Sprintf("location_tile_%d_connected_clients", tileNr)
+	gauge, ok := tileMetrics[name]
+	if !ok {
+		gauge = promauto.NewGauge(prometheus.GaugeOpts{
+			Name: name,
+			Help: "The total number of clients in this tile",
+		})
+		tileMetrics[name] = gauge
+	}
+	gauge.Set(float64(tile.nrTrainers))
+}
+
+func emitNrPokemonsInTile(tileNr int, tile Tile) {
+	name := fmt.Sprintf("location_tile_%d_wild_pokemons", tileNr)
+	gauge, ok := tileMetrics[name]
+	if !ok {
+		gauge = promauto.NewGauge(prometheus.GaugeOpts{
+			Name: name,
+			Help: "The total number of pokemons in this tile",
+		})
+		tileMetrics[name] = gauge
+	}
+	gauge.Set(float64(tile.nrTrainers))
+}
+
+func emitTileMetrics() {
+	tm.activeTiles.Range(func(key, value interface{}) bool {
+		tileNr := key.(int)
+		tile := value.(Tile)
+		emitNrConnectedTrainersInTile(tileNr, tile)
+		emitNrPokemonsInTile(tileNr, tile)
+		return true
+	})
+}
+
 // metrics for prometheus
 func recordMetrics() {
 	go func() {
 		for {
-			counter := 0
-			clientChannels.Range(func(_, _ interface{}) bool {
-				counter++
-				return true
-			})
-
-			connectedClients.Set(float64(counter))
+			emitNrConnectedClients()
+			emitTileMetrics()
 			time.Sleep(8 * time.Second)
 		}
 	}()
