4b44ebdcf9580d79cacc0c0b4e75b29ad38769c7
Nuno Morais <nm.morais@campus.fct.unl.pt>
2020-07-02 09:44:15 +0100

    remove unnecessary locks

--- activeCell.go
+++ activeCell.go
@@ -29,39 +29,31 @@ func NewActiveCell(id s2.CellID, pokemonCellsLevel int) *ActiveCell {
 }
 
 func (ac *ActiveCell) GetPokemonsInCell() []utils.WildPokemonWithServer {
-	ac.cellMutex.RLock()
 	var wildPokemons []utils.WildPokemonWithServer
 	ac.wildPokemons.Range(func(_, pokemon interface{}) bool {
 		wildPokemons = append(wildPokemons, *pokemon.(pokemonsInCellValueType))
 		return true
 	})
-	ac.cellMutex.RUnlock()
 	return wildPokemons
 }
 
 func (ac *ActiveCell) RemovePokemon(wildPokemon utils.WildPokemonWithServer) (ok bool) {
-	ac.cellMutex.RLock()
 	pokemonCell := s2.CellIDFromLatLng(wildPokemon.Location)
 	if _, ok := ac.wildPokemons.Load(pokemonCell); ok {
 		ac.wildPokemons.Delete(pokemonCell)
 	}
-	ac.cellMutex.RUnlock()
 	return ok
 }
 
 func (ac *ActiveCell) ExistsPokemon(wildPokemon utils.WildPokemonWithServer) (ok bool) {
-	ac.cellMutex.RLock()
 	pokemonCell := s2.CellIDFromLatLng(wildPokemon.Location)
 	_, ok = ac.wildPokemons.Load(pokemonCell)
-	ac.cellMutex.RUnlock()
 	return ok
 }
 
 func (ac *ActiveCell) GetPokemon(wildPokemon utils.WildPokemonWithServer) (*utils.WildPokemonWithServer, bool) {
-	ac.cellMutex.RLock()
 	pokemonCell := s2.CellIDFromLatLng(wildPokemon.Location)
 	pokemon, ok := ac.wildPokemons.Load(pokemonCell)
-	ac.cellMutex.RUnlock()
 	if ok {
 		toReturn := pokemon.(pokemonsInCellValueType)
 		return toReturn, ok
@@ -71,12 +63,10 @@ func (ac *ActiveCell) GetPokemon(wildPokemon utils.WildPokemonWithServer) (*util
 }
 
 func (ac *ActiveCell) AddPokemons(wildPokemons []utils.WildPokemonWithServer) {
-	ac.cellMutex.RLock()
 	for _, wildPokemon := range wildPokemons {
 		pokemonCell := s2.CellIDFromLatLng(wildPokemon.Location)
 		ac.wildPokemons.LoadOrStore(pokemonCell, wildPokemon)
 	}
-	ac.cellMutex.RUnlock()
 }
 
 // TODO is this lock necessary
